c050c873e7c39a69d2b0b4e015934a37da231889
xienli <ilmumochi@gmail.com>
2021-10-23 16:12:14 +0700

    forgot something lol

--- drivers/cpufreq/cpufreq_zzmoove.c
+++ drivers/cpufreq/cpufreq_zzmoove.c
@@ -32,12 +32,12 @@
 // #define DISABLE_POWER_MANAGEMENT
 
 // AP: use msm8974 lcd status notifier
-#define USE_LCD_NOTIFIER
+//#define USE_LCD_NOTIFIER
 
 #include <linux/cpu.h>
-#ifdef USE_LCD_NOTIFIER
-#include <linux/lcd_notify.h>
-#endif
+//#ifdef USE_LCD_NOTIFIER
+//#include <linux/lcd_notify.h>
+//#endif
 #include <linux/cpufreq.h>
 #if defined(CONFIG_HAS_EARLYSUSPEND) && !defined (DISABLE_POWER_MANAGEMENT)
 #include <linux/earlysuspend.h>
